d3bbf4a807fd2f453f55c0cc420724e154915263
aliamanuba <31815005+TuxPenguin09@users.noreply.github.com>
2021-09-29 13:41:56 +0800

    Modal Model done

--- src/interfaces/Modal.js
+++ src/interfaces/Modal.js
@@ -1,6 +1,16 @@
 import { Component } from "react";
 import './Modal.css';
 
+/**
+ * Prompt Window
+ * @constructor
+ * @param {string} title - Prompt Title.
+ * @param {string} desc - Prompt Description.
+ * @param {callback} accept - Accept Button Callback.
+ * @param {string} acceptText - Accept Button Text.
+ * @param {callback} reject - Reject Button Callback.
+ * @param {string} rejectText - Reject Button Text.
+ */
 class Prompt extends Component {
     constructor(props) {
         super(props);
@@ -13,7 +23,13 @@ class Prompt extends Component {
         return (
             <span style={{zIndex: 100}}>
                 <div className="Modal-Prompt">
+                    <div id="Modal-Title">{this.props.title}</div>
+                    <div id="Modal-Description">{this.props.desc}</div>
 
+                    <div id="Modal-ButtonContainer">
+                        <div id="Modal-ButtonAccept" onClick={() => this.props.accept()}>{this.props.acceptText}</div>
+                        <div id="Modal-ButtonReject" onClick={() => this.props.reject()}>{this.props.rejectText}</div>
+                    </div>
                 </div>
                 <div className="Modal-BG" />
             </span>
