144ce17d3dcb6364a73c7af6433ccc14d1c3aa97
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2022-02-06 11:24:48 +0800

    Search Bar

--- src/misc/header/header.js
+++ src/misc/header/header.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { ReactComponent as WebLogo } from '../../assets/hodots-logo/logo.svg';
 import './header.css'
+import './search.css'
 
 class Header extends Component {
     constructor(props) {
@@ -15,10 +16,18 @@ class Header extends Component {
             <div style={{ zIndex: 500 }}>
                 <div className="header">
                     <WebLogo className="logo" />
+                    <span>{Search()}</span>
                 </div>
             </div>
         )
     }
+}
+
+function Search(props) {
+    return (<div id="search_bar_container">
+        <input id="search_bar_main" type={"text"} autoCapitalize={false} autoCorrect={false} autoComplete={false} placeholder={"Search..."} />
+        <input id="search_button_main" type={"button"} value={"Search"} />
+    </div>)
 }
 
 export default Header;
