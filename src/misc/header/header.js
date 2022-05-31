d8b8e2bb0aa5e74df106670373cd409e4fe072a7
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2022-06-01 07:32:51 +0800

    Changing Header to Function and Search Resize

--- src/misc/header/header.js
+++ src/misc/header/header.js
@@ -1,26 +1,16 @@
-import { Component } from "react";
 import { ReactComponent as WebLogo } from '../../assets/hodots-logo/logo.svg';
 import './header.css'
 import './search.css'
 
-class Header extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        }
-    }
-
-    render() {
-        return (
-            <div style={{ zIndex: 500 }}>
-                <div className="header">
-                    <WebLogo className="logo" />
-                    <span>{Search()}</span>
-                </div>
+function Header(props) {
+    return (
+        <div style={{ zIndex: 500 }}>
+            <div className="header">
+                <WebLogo className="logo" />
+                <span>{Search()}</span>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 function Search(props) {
